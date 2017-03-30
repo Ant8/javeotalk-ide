7db6a83fb94e139d236c9897f434f032f0c25d47
Ant8 <antonem@o2.pl>
2017-03-30 03:32:52 +0200

    klasa do wywalenia

---
+++ src/javeo/eu/UselessClass.java
@@ -0,0 +1,11 @@
+package javeo.eu;
+
+public class UselessClass {
+    public static void uselessStaticMethod() {
+        notSoUselessMethod();
+    }
+
+    private static void notSoUselessMethod() {
+
+    }
+}
